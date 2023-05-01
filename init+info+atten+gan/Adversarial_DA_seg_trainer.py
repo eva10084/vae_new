674f7c08204bfe3f07f502b41dcf7eb8493396fd
eva10084 <128120727@qq.com>
2023-05-01 21:20:24 +0800

    5.1

--- init+info+atten+gan/Adversarial_DA_seg_trainer.py
+++ init+info+atten+gan/Adversarial_DA_seg_trainer.py
@@ -26,9 +26,6 @@ palette = sns.color_palette("bright", 2)
 '''
 
 EPOCH = 30  # 轮数
-KLDLamda = 1.0   # Kullback-Leibler散度的权重
-PredLamda=1e3
-DisLamda=1e-4
 LR = 1e-3         # 代表Adam优化器的初始学习率
 WEIGHT_DECAY =1e-5   # 代表Adam优化器的权重衰减系数
 WORKERSNUM = 0
@@ -44,12 +41,14 @@ SAVE_DIR=prefix+'/save_model'   # 保存参数路径
 
 
 BatchSize = 4  # 代表每个批次的样本数
-KERNEL = 4   # 代表卷积核的大小
+KERNEL = 4
 
 alpha = 1e0
 beta = 1e-3
 gama = 1e-3
-InfoLamda = 1e2
+infolamda = 1e-1
+kldlamda = 1.0   # Kullback-Leibler散度的权重
+predlamda=1e3
 
 
 
@@ -65,7 +64,7 @@ else:
 if not os.path.exists(prefix):
     os.mkdir(prefix)
 
-def ADA_Train(Infonet, discrim, discrim_criter, discrim_optimizer, source_vae_loss_list,source_seg_loss_list,target_vae_loss_list,distance_loss_list, Train_LoaderA,Train_LoaderB,encoder,decoderA,decoderAdown2,decoderAdown4,decoderB,decoderBdown2,decoderBdown4,gate,DistanceNet,lr,kldlamda,predlamda,infolamda,epoch,optim, savedir):
+def ADA_Train(Infonet, discrim, discrim_criter, discrim_optimizer, source_vae_loss_list,source_seg_loss_list,target_vae_loss_list,distance_loss_list, Train_LoaderA,Train_LoaderB,encoder,decoderA,decoderAdown2,decoderAdown4,decoderB,decoderBdown2,decoderBdown4,gate,DistanceNet,lr,epoch,optim, savedir):
     source_label = 0
     target_label = 1
     lr = lr*(0.9**(epoch))
@@ -124,7 +123,7 @@ def ADA_Train(Infonet, discrim, discrim_criter, discrim_optimizer, source_vae_lo
         segdown4_criterian = segdown4_criterian.to(device)
         segdown4loss_output = segdown4_criterian(outdown4_ct, label_down4)
 
-        recon_ct = decoderA(feat_ct, label_onehot)  # feat_ct为64
+        recon_ct = decoderA(feat_ct, label_onehot)
         BCE_ct = F.binary_cross_entropy(recon_ct, ct)
         KLD_ct = -0.5 * torch.mean(1 + logvar_ct - mu_ct.pow(2) - logvar_ct.exp())
 
@@ -191,25 +190,22 @@ def ADA_Train(Infonet, discrim, discrim_criter, discrim_optimizer, source_vae_lo
 
 
 def SegNet_vali(dir, SegNet, gate,epoch, save_DIR): # gate=0
-    criterion = 0
 
-    # 在指定的dir目录下搜索所有以manual.nii结尾的文件，并将其路径名列表存储在labsname变量中
     labsname = glob.glob(dir + '*manual.nii*')
 
-    total_dice = np.zeros((4,))  # 初始化总的dice系数为0
-    total_Iou = np.zeros((4,))   # 初始化总的IoU系数为0
+    total_dice = np.zeros((4,))
+    total_Iou = np.zeros((4,))
 
-    total_overlap =np.zeros((1,4, 5))   # 初始化总的交集数量为0
-    total_surface_distance=np.zeros((1,4, 5))  # 初始化总的表面距离为0
+    total_overlap =np.zeros((1,4, 5))
+    total_surface_distance=np.zeros((1,4, 5))
 
     num = 0
-    SegNet.eval()   # 设定网络为评估模式，这意味着网络不进行反向传播和梯度更新操作。
+    SegNet.eval()
 
-    # 读取文件夹中的某个病例的MRI图像和对应的分割标签，将标签转换为分类标签并将图像和标签分别保存在npimg和nplab变量中
     for i in range(len(labsname)):
-        itklab = sitk.ReadImage(labsname[i].replace('\\', '/'))   # 读取第i个病例的分割标签。
-        nplab = sitk.GetArrayFromImage(itklab)   # 将读取到的标签转换为NumPy数组。
-        nplab = (nplab == 200) * 1 + (nplab == 500) * 2 + (nplab == 600) * 3 # 将标签的像素值200、500、600分别映射为分类标签1、2、3
+        itklab = sitk.ReadImage(labsname[i].replace('\\', '/'))
+        nplab = sitk.GetArrayFromImage(itklab)
+        nplab = (nplab == 200) * 1 + (nplab == 500) * 2 + (nplab == 600) * 3
 
         imgname = labsname[i].replace('_manual.nii', '.nii')
         itkimg = sitk.ReadImage(imgname.replace('\\', '/'))
@@ -221,7 +217,7 @@ def SegNet_vali(dir, SegNet, gate,epoch, save_DIR): # gate=0
         label = torch.from_numpy(nplab).to(device)
         truearg  = np.zeros((data.size(0), data.size(2), data.size(3)))
 
-        # 对每个切片进行操作
+
         for slice in range(data.size(0)):
             output,_,_, _, _, _ ,_,_,_,_,_,_,_,_,_,_,_= SegNet(data[slice:slice+1,:,:,:], gate)
 
@@ -257,9 +253,6 @@ def SegNet_vali(dir, SegNet, gate,epoch, save_DIR): # gate=0
         mean_surface_distance = np.mean(total_surface_distance[1:], axis=0)
         std_surface_distance = np.std(total_surface_distance[1:], axis=0)
 
-        # print(dir)
-        # 判断是验证集还是测试集
-
         criterion = np.mean(meanDice[1:])
         phase='validate'
 
@@ -270,77 +263,56 @@ def SegNet_vali(dir, SegNet, gate,epoch, save_DIR): # gate=0
                              ,str(meanDice.tolist()),"stdDice:","",str(stdDice.tolist()),"","\n","meanIou:","",str(meanIou.tolist()),"stdIou:","",str(stdIou.tolist()), \
                               "", "\n\n","jaccard, dice, volume_similarity, false_negative, false_positive:", "\n","mean:", str(mean_overlap.tolist()),"\n", "std:", "", str(std_overlap.tolist()), \
                               "", "\n\n","hausdorff_distance, mean_surface_distance, median_surface_distance, std_surface_distance, max_surface_distance:", "\n","mean:", str(mean_surface_distance.tolist()), "\n","std:", str(std_surface_distance.tolist())])
-    # 用四个dice的均值做指标
     return criterion
 
 
-# 该函数的功能是进行t-SNE映射并生成散点图。
 def t_SNE_plot(Train_LoaderA,Train_LoaderB,net,save_dir,mode):
-    # Train_LoaderA：源域数据集的dataloader；Train_LoaderB：目标域数据集的dataloader；
-    # net：VAE - UNet网络；ave_dir：保存文件的目录；mode：保存文件的前缀名。
 
-    # 分别从两个数据集中获取训练数据
     A_iter = iter(Train_LoaderA)
     B_iter = iter(Train_LoaderB)
     print(len(Train_LoaderA))
     print(len(Train_LoaderB))
     net.eval()
 
-    # 初始化特征向量
     features_A = np.zeros((64,))
     features_B = np.zeros((64,))
 
 
     num_iter = min(len(Train_LoaderA) - 1, len(Train_LoaderB) - 1)
     for i in tqdm(range(num_iter)):
-        # 从两个数据集中获取训练数据
         sour, sour_down2, sour_down4, label, label_down2, label_down4, info_sour = next(A_iter)
         tar, tar_down2, tar_down4, info_tar = next(B_iter)
 
-        # 将数据传输到GPU上进行计算
         sour = sour.to(device)
         tar = tar.to(device)
 
-        # 进行特征提取
         _, _, _, feat_sour, _, _, _, _, _, _, _, _, _, _, _, _, _ = net(sour, 0.0)
         _, _, _, feat_tar, _, _, _, _, _, _, _, _, _, _, _, _, _  = net(tar, 0.0)
 
-        # 将特征向量加入到列表中
         features_A = np.vstack((features_A, feat_sour.cpu().detach().numpy().mean(axis=(2,3)).reshape(sour.size(0),-1)))
         features_B = np.vstack((features_B, feat_tar.cpu().detach().numpy().mean(axis=(2,3)).reshape(tar.size(0),-1)))
 
 
-    # 对特征向量进行t-SNE降维
     tsne = TSNE()
-    # print ('tsne class')
-    # print (features_A.shape)
     X_embedded = tsne.fit_transform(np.concatenate((features_A[1:], features_B[1:]),axis=0))
-    # print ('finish mapping')
     Y = ['source']*features_A[1:].shape[0]+['target']*features_A[1:].shape[0]
-    # Y = ['source'] * 500 + ['target'] * 500
 
-    # 可视化结果，有问题
     tag = features_A[1:].shape[0]
     colors = ['green' if y == 'source' else 'blue' for y in Y]
     plt.scatter(X_embedded[:tag, 0], X_embedded[:tag, 1], c=colors[:tag])
     plt.scatter(X_embedded[tag:, 0], X_embedded[tag:, 1], c=colors[tag:])
-    # sns.scatterplot(X_embedded[:, 0], X_embedded[:, 1], hue=Y, legend='full', palette=palette)
     plt.savefig(os.path.join(save_dir, '{}.png'.format(mode)))
     plt.close()
-    #
     # # 将t-SNE结果保存到文件中
     # np.save(os.path.join(save_dir, '{}_X.npy'.format(mode)), X_embedded)
     # np.save(os.path.join(save_dir, '{}_Y.npy'.format(mode)), np.array(Y))
 
 
-
-# 初始化权值
 def init_weights(m):
     if isinstance(m, nn.Conv2d) or isinstance(m,nn.ConvTranspose2d):
         nn.init.xavier_uniform_(m.weight)
         m.bias.data.fill_(0.01)
 
-# 将源域的VAE的loss，分割器的loss，目标域的VAE的loss，和距离loss，这四个list转化为numpy，并将它们分别保存到SAVE_DIR指定目录下的四个.npy文件中
 def show_loss(source_vae_loss_list,source_seg_loss_list,target_vae_loss_list,distance_loss_list,SAVE_DIR):
     print('\n iter num: %d' % (len(source_vae_loss_list)))
     np.save(os.path.join(SAVE_DIR, 'source_bce_loss_list.npy'), source_vae_loss_list)
@@ -348,25 +320,19 @@ def show_loss(source_vae_loss_list,source_seg_loss_list,target_vae_loss_list,dis
     np.save(os.path.join(SAVE_DIR, 'target_bce_loss_list.npy'), target_vae_loss_list)
     np.save(os.path.join(SAVE_DIR, 'distance_loss_list.npy'), distance_loss_list)
 
-    # 第一个参数x轴数据，第二个参数y轴数据，第三个参数指定折线的颜色，第四个参数标注
-    # 将source_vae_loss_list数组中的值绘制成一条红色实线
     plt.plot(np.arange(0, source_vae_loss_list.shape[0]),
              source_vae_loss_list, 'r', linestyle="-",
              label=r'$\widetilde{\mathcal{L}}_{S/seg}$')
-    # 将source_seg_loss_list数组中的值绘制成一条蓝色实线
     plt.plot(np.arange(0, source_seg_loss_list.shape[0]), source_seg_loss_list,
              'b',
              label=r'$\widetilde{\mathcal{L}}_{S:seg}$')
-    # 将target_vae_loss_list数组中的值绘制成一条绿色实线
     plt.plot(np.arange(0, target_vae_loss_list.shape[0]),
              target_vae_loss_list, 'g', linestyle="-",
              label=r'$\widetilde{\mathcal{L}}_{T}$')
-    # 将distance_loss_list数组中的值绘制成一条品红色实线
     plt.plot(np.arange(0, distance_loss_list.shape[0]), distance_loss_list,
              'm',
              label=r'$\widetilde{\mathcal{D}}$')
 
-    # 添加了一个图例，以及对应的坐标系相关数据
     plt.legend()
     plt.xlabel('iterations', fontsize=15)
     plt.ylabel(" Losses", fontsize=15)
@@ -376,9 +342,7 @@ def show_loss(source_vae_loss_list,source_seg_loss_list,target_vae_loss_list,dis
     plt.close()
 
 def model_init():
-    # 定义多个解码器模型，包括 source_vaedecoder、source_down2_vaedecoder、source_down4_vaedecoder、target_vaedecoder、target_down2_vaedecoder 和 target_down4_vaedecoder
-
-    vaeencoder = VAE()  # 定义 VAE 编码器模型
+    vaeencoder = VAE()
     vaeencoder = vaeencoder.to(device)
 
     source_vaedecoder = VAEDecode()
@@ -410,9 +374,7 @@ def main():
     os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     cudnn.benchmark = True
 
-
     vaeencoder,source_vaedecoder, source_down2_vaedecoder, source_down4_vaedecoder ,target_vaedecoder,target_down2_vaedecoder, target_down4_vaedecoder, discrim, Infonet= model_init()
-
 
     DistanceNet = Gaussian_Distance(KERNEL)
     DistanceNet = DistanceNet.to(device)
@@ -472,11 +434,12 @@ def main():
 
         # 进行训练，调用程序
         ADA_Train(Infonet, discrim, discrim_criter, discrim_optimizer,
-                  source_vae_loss_list,source_seg_loss_list,target_vae_loss_list,distance_loss_list, SourceData_loader,
-                  TargetData_loader,vaeencoder,
-                  source_vaedecoder,source_down2_vaedecoder,source_down4_vaedecoder,
-                  target_vaedecoder,target_down2_vaedecoder,target_down4_vaedecoder,
-                  1.0,DistanceNet,LR,KLDLamda,PredLamda,InfoLamda,epoch,DA_optim, SAVE_DIR)
+                  source_vae_loss_list, source_seg_loss_list, target_vae_loss_list, distance_loss_list,
+                  SourceData_loader, TargetData_loader,
+                  vaeencoder,
+                  source_vaedecoder, source_down2_vaedecoder, source_down4_vaedecoder,
+                  target_vaedecoder, target_down2_vaedecoder, target_down4_vaedecoder,
+                  1.0, DistanceNet, LR, epoch, DA_optim, SAVE_DIR)
 
         vaeencoder.eval()
         criter =SegNet_vali(ValiDir, vaeencoder, 0, epoch, SAVE_DIR)
@@ -517,7 +480,6 @@ def main():
 
     show_loss(np.array(source_vae_loss_list), np.array(source_seg_loss_list),
               np.array(target_vae_loss_list) ,np.array(distance_loss_list), SAVE_DIR)
-
 
 
 
