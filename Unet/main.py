f448383b9236a12cad448f94c3b1da964b18a602
eva10084 <128120727@qq.com>
2023-05-03 16:44:30 +0800

    5.3

--- Unet/main.py
+++ Unet/main.py
@@ -75,83 +75,6 @@ def index_to_image(image):
   return image
 
 
-# 验证集
-def SegNet_vali(dir, SegNet, gate,epoch, save_DIR):
-
-    labsname = glob.glob(dir + '*manual.nii*')
-
-    total_dice = np.zeros((4,))
-    total_Iou = np.zeros((4,))
-
-    total_overlap =np.zeros((1,4, 5))
-    total_surface_distance=np.zeros((1,4, 5))
-
-    num = 0
-    SegNet.eval()
-
-    for i in range(len(labsname)):
-        itklab = sitk.ReadImage(labsname[i].replace('\\', '/'))
-        nplab = sitk.GetArrayFromImage(itklab)
-        nplab = (nplab == 200) * 1 + (nplab == 500) * 2 + (nplab == 600) * 3
-
-        imgname = labsname[i].replace('_manual.nii', '.nii')
-        itkimg = sitk.ReadImage(imgname.replace('\\', '/'))
-        npimg = sitk.GetArrayFromImage(itkimg)
-        npimg = npimg.astype(np.float32)
-
-
-        data=torch.from_numpy(np.expand_dims(npimg,axis=1)).type(dtype=torch.FloatTensor).to(device)
-        label = torch.from_numpy(nplab).to(device)
-        truearg  = np.zeros((data.size(0), data.size(2), data.size(3)))
-
-
-        for slice in range(data.size(0)):
-            output,_,_, _, _, _ ,_,_,_,_,_,_,_,_,_,_,_= SegNet(data[slice:slice+1,:,:,:], gate)
-
-            truemax, truearg0 = torch.max(output, 1, keepdim=False)
-            truearg0 = truearg0.detach().cpu().numpy()
-            truearg[slice:slice+1,:,:]=truearg0
-
-
-        dice = dice_compute(truearg,label.cpu().numpy())
-        Iou = IOU_compute(truearg,label.cpu().numpy())
-        overlap_result, surface_distance_result = Hausdorff_compute(truearg,label.cpu().numpy(),itkimg.GetSpacing())
-
-        total_dice = np.vstack((total_dice, dice))
-        total_Iou = np.vstack((total_Iou, Iou))
-
-        total_overlap = np.concatenate((total_overlap,overlap_result),axis=0)
-        total_surface_distance = np.concatenate((total_surface_distance,surface_distance_result),axis=0)
-
-        num+=1
-
-    if num==0:
-        return
-    else:
-        meanDice = np.mean(total_dice[1:],axis=0)
-        stdDice = np.std(total_dice[1:],axis=0)
-
-        meanIou = np.mean(total_Iou[1:],axis=0)
-        stdIou = np.std(total_Iou[1:],axis=0)
-
-        mean_overlap = np.mean(total_overlap[1:], axis=0)
-        std_overlap = np.std(total_overlap[1:], axis=0)
-
-        mean_surface_distance = np.mean(total_surface_distance[1:], axis=0)
-        std_surface_distance = np.std(total_surface_distance[1:], axis=0)
-
-        criterion = np.mean(meanDice[1:])
-        phase='validate'
-
-        print("epoch:", epoch, " ", "meanDice:", meanDice, " ", "meanIOU:", meanIou)
-
-        with open("%s/validate_infomation.txt" % (save_DIR), "a") as f:
-            f.writelines(["\n\nepoch:", str(epoch), " ",phase," ", "\n","meanDice:",""\
-                             ,str(meanDice.tolist()),"stdDice:","",str(stdDice.tolist()),"","\n","meanIou:","",str(meanIou.tolist()),"stdIou:","",str(stdIou.tolist()), \
-                              "", "\n\n","jaccard, dice, volume_similarity, false_negative, false_positive:", "\n","mean:", str(mean_overlap.tolist()),"\n", "std:", "", str(std_overlap.tolist()), \
-                              "", "\n\n","hausdorff_distance, mean_surface_distance, median_surface_distance, std_surface_distance, max_surface_distance:", "\n","mean:", str(mean_surface_distance.tolist()), "\n","std:", str(std_surface_distance.tolist())])
-    return criterion
-
 def show(model,epoch):
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(10, 5))
     name = dataset_dir+'/'+source+'/'+'patient1_C0.nii'
@@ -210,28 +133,18 @@ def main():
     if not os.path.exists(SAVE_DIR):  # 如果保存训练结果的目录不存在，则创建该目录
         os.mkdir(SAVE_DIR)
 
-    criterion = 0
-    best_epoch = 0
+
     for epoch in range(EPOCH):
         # 设置为训练模式
         model.train()
 
         for image, label in tqdm(dataloader1):
             image = image.to(device)
-            # print(image.shape)
-            # [print(i) for i in image[0]]
             label = label.to(device, dtype=torch.int64)  # 需要int64参与运算
-            # print(label.shape)
-            # [print(i) for i in label[0,:,80,80]]
             label_onehot = one_hot(label)
 
-            # print(label_onehot.shape)
-            # [print(i) for i in label_onehot[0,:,80,80]]
-
-
             out = model(image)
-            # print(out.shape)
-            # [print(i) for i in out[0, :, 80, 80]]
+
             loss = nn.BCELoss()(out.float(), label_onehot.float())
             # loss = dice_coefficient(out.float(), label_onehot.float())
 
@@ -239,27 +152,15 @@ def main():
             loss.backward()
             optimizer.step()
 
-            dice = show(model, epoch)
-            mean_dice = (dice[0]+dice[1]+dice[2]+dice[3])/4
-            if mean_dice >= 0.7:
-                torch.save(model.state_dict(), SAVE_DIR + '/dice_' + str(mean_dice) + '.pkl')
-
-
+        dice = show(model, epoch)
+        mean_dice = (dice[0]+dice[1]+dice[2]+dice[3])/4
+        if mean_dice >= 0.7:
+            torch.save(model.state_dict(), SAVE_DIR + '/dice_' + str(mean_dice) + '.pkl')
 
         print(f"\nEpoch: {epoch}/{EPOCH}, Loss: {loss}, dice:{dice}")
         torch.save(model.state_dict(), SAVE_DIR+'/res.pkl')
 
 
-'''
-            x = image[0]  # 原图
-            x_ = index_to_image(from_onehot_to_label(out[0]))  # 运行
-            y = index_to_image(label[0])   # 标注
-            # [print(i) for i in y]
-            print(x.shape,x_.shape,y.shape)
-            img = torch.stack([x, x_, y], 0)
-            save_image(img.cpu(), "kk.png")
-            # exit()
-'''
 
 if __name__ == '__main__':
     main()
